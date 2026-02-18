c4d0e3f4b5777f44d354bef1ad64bbd52d2bab73
zili <neodejack@gmail.com>
2026-02-18 12:55:52 +0800

    support elixir 1.16 and above

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Notex.MixProject do
     [
       app: :notex,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
