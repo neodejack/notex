f22e1d70732bc9c10e64aa4f0e1dd3ac784361ef
zili <neodejack@gmail.com>
2026-03-07 15:18:39 +0800

    delete chord api draft

--- lib/notex/chord.ex
+++ lib/notex/chord.ex
@@ -14,33 +14,6 @@ defmodule Notex.Chord do
   @enforce_keys [:base_note, :voicings]
   defstruct [:base_note, :voicings]
 
-  @spec new(Note.t()) :: t()
-  def new(%Note{} = base_note) do
-    add(%Chord{base_note: base_note, voicings: []}, :one, 0)
-  end
-
-  @spec add(t(), Constant.interval_id(), octave_offset()) :: t()
-  def add(%Chord{voicings: voicings} = chord, interval, octave_offset) do
-    updated_voicings =
-      Keyword.update(voicings, interval, [octave_offset], &Enum.uniq([octave_offset | &1]))
-
-    %{chord | voicings: updated_voicings}
-  end
-
-  @spec omit(t(), Constant.interval_id(), octave_offset()) :: t()
-  def omit(%Chord{voicings: voicings} = chord, interval, octave_offset) do
-    case Keyword.fetch(voicings, interval) do
-      :error ->
-        chord
-
-      {:ok, octave_offsets} ->
-        case List.delete(octave_offsets, octave_offset) do
-          [] -> %{chord | voicings: Keyword.delete(voicings, interval)}
-          remaining_offsets -> %{chord | voicings: Keyword.put(voicings, interval, remaining_offsets)}
-        end
-    end
-  end
-
   @spec notes(t()) :: {:ok, [Note.t()]} | {:error, binary()}
   def notes(%Chord{base_note: base_note, voicings: voicings}) do
     interval_semitones = Constant.interval_semitones()
@@ -70,12 +43,5 @@ defmodule Notex.Chord do
 
   defp octave_semitones(octave) when is_integer(octave) do
     octave * 12
-  end
-
-  def maj(%Note{} = base_note) do
-    base_note
-    |> new()
-    |> add(:three, 0)
-    |> add(:five, 0)
   end
 end
